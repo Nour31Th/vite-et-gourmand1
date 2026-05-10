a487263d1d65e1c3692af685536d1d6a1797168e
miorath <thualmiora.31@gmail.com>
2026-05-10 18:32:16 +0200

    feat: page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ContactController extends AbstractController
+class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
-    {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+    public function index(
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        if ($request->isMethod('POST')) {
+            $contact = new Contact();
+            $contact->setTitre($request->request->get('titre'));
+            $contact->setDescription($request->request->get('description'));
+            $contact->setEmail($request->request->get('email'));
+            $contact->setDate(new \DateTime());
+
+            $em->persist($contact);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre message a bien été envoyé ! Nous vous répondrons dans les plus brefs délais.');
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/index.html.twig');
     }
 }
