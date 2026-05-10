f47b747dcb04238d9e62daad447f4d5dfc80f07b
miorath <thualmiora.31@gmail.com>
2026-05-10 17:47:21 +0200

    feat: espace employe + gestion commandes + validation avis

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,17 +2,100 @@
 
 namespace App\Controller;
 
+use App\Entity\HistoriqueStatut;
+use App\Repository\CommandeRepository;
+use App\Repository\AvisRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-final class EmployeController extends AbstractController
+#[IsGranted('ROLE_EMPLOYE')]
+#[Route('/employe')]
+class EmployeController extends AbstractController
 {
-    #[Route('/employe', name: 'app_employe')]
-    public function index(): Response
+    #[Route('', name: 'app_employe_dashboard')]
+    public function dashboard(CommandeRepository $commandeRepository): Response
     {
-        return $this->render('employe/index.html.twig', [
-            'controller_name' => 'EmployeController',
+        $commandes = $commandeRepository->findBy([], ['date_commande' => 'DESC']);
+
+        return $this->render('employe/dashboard.html.twig', [
+            'commandes' => $commandes,
         ]);
+    }
+
+    #[Route('/commande/{id}/statut', name: 'app_employe_update_statut', methods: ['POST'])]
+    public function updateStatut(
+        int $id,
+        Request $request,
+        EntityManagerInterface $em,
+        CommandeRepository $commandeRepository
+    ): Response {
+        $commande = $commandeRepository->find($id);
+
+        if (!$commande) {
+            throw $this->createNotFoundException();
+        }
+
+        $nouveauStatut = $request->request->get('statut');
+        $commentaire = $request->request->get('commentaire');
+        $modeContact = $request->request->get('mode_contact');
+
+        $commande->setStatut($nouveauStatut);
+
+        $historique = new HistoriqueStatut();
+        $historique->setCommande($commande);
+        $historique->setStatut($nouveauStatut);
+        $historique->setDateHeure(new \DateTime());
+        $historique->setCommentaire($commentaire);
+        $historique->setModeContact($modeContact);
+
+        $em->persist($historique);
+        $em->flush();
+
+        $this->addFlash('success', 'Statut mis à jour avec succès.');
+        return $this->redirectToRoute('app_employe_dashboard');
+    }
+
+    #[Route('/avis', name: 'app_employe_avis')]
+    public function avis(AvisRepository $avisRepository): Response
+    {
+        $avis = $avisRepository->findBy(['valide' => false], ['date_avis' => 'DESC']);
+
+        return $this->render('employe/avis.html.twig', [
+            'avis' => $avis,
+        ]);
+    }
+
+    #[Route('/avis/{id}/valider', name: 'app_employe_valider_avis', methods: ['POST'])]
+    public function validerAvis(
+        int $id,
+        AvisRepository $avisRepository,
+        EntityManagerInterface $em
+    ): Response {
+        $avi = $avisRepository->find($id);
+        if ($avi) {
+            $avi->setValide(true);
+            $em->flush();
+            $this->addFlash('success', 'Avis validé.');
+        }
+        return $this->redirectToRoute('app_employe_avis');
+    }
+
+    #[Route('/avis/{id}/refuser', name: 'app_employe_refuser_avis', methods: ['POST'])]
+    public function refuserAvis(
+        int $id,
+        AvisRepository $avisRepository,
+        EntityManagerInterface $em
+    ): Response {
+        $avi = $avisRepository->find($id);
+        if ($avi) {
+            $em->remove($avi);
+            $em->flush();
+            $this->addFlash('success', 'Avis refusé et supprimé.');
+        }
+        return $this->redirectToRoute('app_employe_avis');
     }
 }
